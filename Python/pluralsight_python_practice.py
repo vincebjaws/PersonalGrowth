eb31849939f04350bcee645694e3701774835ea7
vincebjaws <53380034+vincebjaws@users.noreply.github.com>
2022-04-11 10:21:17 -0700

    Creation of pluralsight python practice.

---
+++ Python/pluralsight_python_practice.py
@@ -0,0 +1,5 @@
+#--------------------------------------------------------------
+# General Python Practice and notes for April 11, 2022
+# Core Python: Big Picture By Jason Olson
+# Practice by Vince (Vino) Bjazevic
+#--------------------------------------------------------------
